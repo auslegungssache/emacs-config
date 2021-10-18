925991e9b08bb169d816fc3fee5200dbcf1053b8
aeggy <aeggy@DESKTOP-1TU4OFK.localdomain>
2021-10-18 18:04:29 +0200

    move startup config to org file and extend

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (setq gc-cons-threshold 100000000)
 
-(setq inhibit-startup-screen t)
 (setq frame-resize-pixelwise t)
 (setq ring-bell-function (lambda ()))
 (org-babel-load-file (concat user-emacs-directory "main-config.org"))
