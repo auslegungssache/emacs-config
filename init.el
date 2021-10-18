f8f4aed9edaf5c509b5405d0c5ed631292ce62e2
aeggy <aeggy@DESKTOP-1TU4OFK.localdomain>
2021-10-18 18:35:17 +0200

    remove code duplication in init.el

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
 (setq gc-cons-threshold 100000000)
 
-(setq frame-resize-pixelwise t)
-(setq ring-bell-function (lambda ()))
 (org-babel-load-file (concat user-emacs-directory "main-config.org"))
-(put 'upcase-region 'disabled nil)
