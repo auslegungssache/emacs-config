6db66614ac6cda5b3a8caee5fa2d57b06f40a0bb
aeggy <aeggy@DESKTOP-1TU4OFK.localdomain>
2021-10-28 15:22:38 +0200

    rename main-config to config

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
   (load-file (concat user-emacs-directory "early-init.el")))
 
 (defvar my-tangle-source (expand-file-name "tangle.el" user-emacs-directory))
-(defvar my-emacs-config-org (expand-file-name "main-config.org" user-emacs-directory))
+(defvar my-emacs-config-org (expand-file-name "config.org" user-emacs-directory))
 (defvar my-emacs-config-el (expand-file-name "var/config.el" user-emacs-directory))
 (when (file-newer-than-file-p my-emacs-config-org my-emacs-config-el)
   (load-file my-tangle-source)
