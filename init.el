6a20f95e153f927d75f9d98c6530412577e4b0a1
aeggydev <kucernak.a@gmail.com>
2022-07-11 13:13:05 +0200

    Move config.org to README.org

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
   (load-file (concat user-emacs-directory "early-init.el")))
 
 (defvar my-tangle-source (expand-file-name "tangle.el" user-emacs-directory))
-(defvar my-emacs-config-org (expand-file-name "config.org" user-emacs-directory))
+(defvar my-emacs-config-org (expand-file-name "README.org" user-emacs-directory))
 (defvar my-emacs-config-el (expand-file-name "var/config.el" user-emacs-directory))
 (when (file-newer-than-file-p my-emacs-config-org my-emacs-config-el)
   (load-file my-tangle-source)
